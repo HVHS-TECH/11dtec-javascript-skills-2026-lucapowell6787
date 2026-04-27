15d05803b42f95680a71ace8e4fdc1cdccdc023e
lucapowell6787 <24093cc@hvhs.school.nz>
2026-04-28 10:59:18 +1200

    java stuff

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,9 +1,9 @@
 /****************************
-This is a block comment.
-Put your header comment here!
+introduction yoza
+dont know what to put here yoza
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("yoza planet")
 
 // This is a single line comment
 
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,7 @@
 /* Paste your code from task 1 here */
+var userName = "alice"
+var age = "15"
+var year = "2026"
+var money = "0.79"
+
+console.log("hi " +userName)
